21d1040428aae0e5cf47960f9c1fef3dcb72754b
Pham Son Hoang <pshoang102@gmail.com>
2019-11-02 18:43:06 -0500

    api fixed

--- app.js
+++ app.js
@@ -66,66 +66,65 @@ jsonObj = {
 }
 
 app.get("/", (req, res) => {
-    // request("https://d75dffea.ngrok.io/testuser", {json: true}, (err, response, body) => {
-    //     if(err) {return console.log(err);}
-    //     // Check if response body is empty
-    //     if(JSON.stringify(body) !== "{}") {
-    //         // Populate jsonObj
-    //         jsonObj = body; 
-    //         // {
-    //         //     title: "Your Restaurant Name",
-    //         //     jumboText1: "Your First Text Here",
-    //         //     jumboText2: "And Another One",
-    //         //     address: "123 Hyde Park Blvd",
-    //         //     email: "johndoe123@gmail.com",
-    //         //     phoneNumber: "773 604 9872"
-    //         // };
-    //         jsonObj["entree"] = [
-    //             {
-    //                 name: "Pho Pizza",
-    //                 desc: "A pizza with your favorite ingredients from Pho",
-    //                 price: "8.50",
-    //                 img: "https://cdn.foodbeast.com/content/uploads/2016/06/Pho-Pizza-Cover.jpg"
-    //             },
-    //             {
-    //                 name: "Seafood Delight",
-    //                 desc: "Shrimp, squid, tomato, and family secret",
-    //                 price: "12.00",
-    //                 img: "https://images-gmi-pmc.edge-generalmills.com/57be9c18-f2e5-453c-9286-c96d3ddb9555.jpg"
-    //             },
-    //             {
-    //                 name: "Banh Mi",
-    //                 desc: "Vietnamese Banh Mi",
-    //                 price: "5.00",
-    //                 img: "https://www.happyfoodstube.com/wp-content/uploads/2018/08/vietnamese-sandwich-banh-mi-image-500x500.jpg"
-    //             },
-    //             {
-    //                 name: "Carbonara",
-    //                 desc: "Carbonara spaghetti",
-    //                 price: "7.00",
-    //                 img: "https://www.oliviascuisine.com/wp-content/uploads/2019/01/authentic-spaghetti-carbonara-IG-720x540.jpg"
-    //             }
-    //         ];
-    //         jsonObj["appetizers"] = [
-    //             {
-    //                 name: "Tuna Sesame Salad",
-    //                 desc: "Tuna, sesame, salad",
-    //                 price: "3.75",
-    //                 img: "http://balaboosta.com.au/wp-content/uploads/2015/10/PA070874.jpg"
-    //             },
-    //             {
-    //                 name: "Smoked Salmon",
-    //                 desc: "Salmon, bread, cheese... all on a stick",
-    //                 price: "4.50",
-    //                 img: "https://www.recipetineats.com/wp-content/uploads/2016/10/Smoked-Salmon-Bites_2-680x951.jpg"
-    //             }
-    //         ];
-    //     }
-    //     console.log(jsonObj);
-    //     // Generate dynamic website from jsonObj
-        
-    // });
-    res.render("index", jsonObj);
+    request("https://d75dffea.ngrok.io/testuser", {json: true}, (err, response, body) => {
+        if(err) {return console.log(err);}
+        // Check if response body is empty
+        if(JSON.stringify(body) !== "{}") {
+            // Populate jsonObj
+            jsonObj = body; 
+            // {
+            //     title: "Your Restaurant Name",
+            //     jumboText1: "Your First Text Here",
+            //     jumboText2: "And Another One",
+            //     address: "123 Hyde Park Blvd",
+            //     email: "johndoe123@gmail.com",
+            //     phoneNumber: "773 604 9872"
+            // };
+            jsonObj["entree"] = [
+                {
+                    name: "Pho Pizza",
+                    desc: "A pizza with your favorite ingredients from Pho",
+                    price: "8.50",
+                    img: "https://cdn.foodbeast.com/content/uploads/2016/06/Pho-Pizza-Cover.jpg"
+                },
+                {
+                    name: "Seafood Delight",
+                    desc: "Shrimp, squid, tomato, and family secret",
+                    price: "12.00",
+                    img: "https://images-gmi-pmc.edge-generalmills.com/57be9c18-f2e5-453c-9286-c96d3ddb9555.jpg"
+                },
+                {
+                    name: "Banh Mi",
+                    desc: "Vietnamese Banh Mi",
+                    price: "5.00",
+                    img: "https://www.happyfoodstube.com/wp-content/uploads/2018/08/vietnamese-sandwich-banh-mi-image-500x500.jpg"
+                },
+                {
+                    name: "Carbonara",
+                    desc: "Carbonara spaghetti",
+                    price: "7.00",
+                    img: "https://www.oliviascuisine.com/wp-content/uploads/2019/01/authentic-spaghetti-carbonara-IG-720x540.jpg"
+                }
+            ];
+            jsonObj["appetizers"] = [
+                {
+                    name: "Tuna Sesame Salad",
+                    desc: "Tuna, sesame, salad",
+                    price: "3.75",
+                    img: "http://balaboosta.com.au/wp-content/uploads/2015/10/PA070874.jpg"
+                },
+                {
+                    name: "Smoked Salmon",
+                    desc: "Salmon, bread, cheese... all on a stick",
+                    price: "4.50",
+                    img: "https://www.recipetineats.com/wp-content/uploads/2016/10/Smoked-Salmon-Bites_2-680x951.jpg"
+                }
+            ];
+        }
+        console.log(jsonObj);
+        // Generate dynamic website from jsonObj
+        res.render("index", jsonObj);
+    });
 });
 
 
